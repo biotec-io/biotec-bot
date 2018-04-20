7b3931283ac23bf22b7763920dd58dff0676f61c
Edgar Talledos <onimenotsuki@gmail.com>
2018-04-20 12:46:23 -0500

    GiftedChat example added with success

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+/* eslint camelcase: 0 */
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Header } from 'react-native-elements';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,12 +11,50 @@ const styles = StyleSheet.create({
 });
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  UNSAFE_componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hola, ¿en qué puedo ayudarte?',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://s3.us-east-2.amazonaws.com/biotec-io/robot.png',
+          },
+        },
+      ],
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Header
           backgroundColor="#233978"
           centerComponent={{ text: 'BiotecBot', style: { color: '#fff' } }}
+        />
+
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
         />
       </View>
     );
