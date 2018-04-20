c73f7c260fef36b612f5d79f3f0afe00ba23d1b8
Edgar Talledos <onimenotsuki@gmail.com>
2018-04-20 12:17:06 -0500

    Add header component with react-native-elements

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header
+          backgroundColor="#233978"
+          centerComponent={{ text: 'BiotecBot', style: { color: '#fff' } }}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
